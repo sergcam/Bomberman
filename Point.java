fc4af3e27db60dd575c3a63a3de17c30dde6a5d9
genenam <39537490+genenam@users.noreply.github.com>
2018-05-22 14:13:48 -0700

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,4 +1,13 @@
-
-public class Point {
-
+public class Point{
+    private int x, y;
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
 }
