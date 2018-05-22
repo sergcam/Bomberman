ec4498370fc086b957e3ea5c3d4bff8149d1ea43
genenam <39537490+genenam@users.noreply.github.com>
2018-05-22 14:13:04 -0700

    Update Man.java

--- Man.java
+++ Man.java
@@ -1,4 +1,70 @@
+public class Man{
+    private String name;
+    private int lives;
+    private int x, y, bombStr;
+    private boolean placedBomb;
+    public Man(String name, int x, int y){
+        this.name = name;
+        lives = 3;
+        this.x = x;
+        this.y = y;
+        bombStr = 50;
+        placedBomb = false;
+    }
 
-public class Man {
+    public void moveUp(){
+        y -= 50;
+    }
 
+    public void moveDown(){
+        y += 50;
+    }
+
+    public void moveRight(){
+        x += 50;
+    }  
+
+    public void moveLeft(){
+        x -= 50;
+    }
+    
+    public void increaseLives(){
+        lives++;
+    }
+
+    public void decreaseLives(){
+        lives --;
+    }
+    
+    public int getLives(){
+        return lives;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public int getBombStr(){
+        return bombStr;
+    }
+    
+    public void increaseBombStr(){
+        bombStr += 50;
+    }
+    
+    public void setBombStr(int x){
+        bombStr = x;
+    }
+    
+    public void placeBomb(boolean x){
+        placedBomb = x;
+    }
+    
+    public boolean isPlaced(){
+        return placedBomb;
+    }
 }
