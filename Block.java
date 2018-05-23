fe454b9538a57b4f3b85c6eaf7d549cfbc690346
genenam <39537490+genenam@users.noreply.github.com>
2018-05-23 09:53:32 -0700

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,4 +1,11 @@
-
-public class Block {
-
+public class Block{
+    private int x, y, item;
+    public Block(int x, int y){
+        this.x = x;
+        this.y = y;
+        item = (int)Math.random() * 4;
+    }
+    public int destroy(){
+        return item;
+    }
 }
