2a81f7c26c0704501fb19777e701e29ede75ea7b
genenam <39537490+genenam@users.noreply.github.com>
2018-05-23 09:55:30 -0700

    Update Bomb.java

--- Bomb.java
+++ Bomb.java
@@ -1,21 +1,32 @@
 import java.util.List;
 import java.util.ArrayList;
 public class Bomb{
-    private int blastRadius, x, y;
+    private int blastRadius, x, y, time;
     public Bomb(int b, int x, int y){
         blastRadius = b;
         this.x = x;
         this.y = y;
+        time = 0;
     }
     //returns Points of affected tiles based on initial point and blastRadius
     public List<Point> explode(){
         List cool = new ArrayList<Point>();
-        for(int i = x - blastRadius; i < blastRadius + x; i++){
+        for(int i = x; i >= x - blastRadius; i -= 50){
             if(i != x){
                 cool.add(new Point(i, y));
             }
         }
-        for(int i = y - blastRadius; i < blastRadius + y; i++){
+        for(int i = x; i <= x + blastRadius; i += 50){
+            if(i != x){
+                cool.add(new Point(i, y));
+            }
+        }
+        for(int i = y; i >= y - blastRadius; i -= 50){
+            if(i != y){
+                cool.add(new Point(x, i));
+            }
+        }
+        for(int i = y; i <= y + blastRadius; i += 50){
             if(i != y){
                 cool.add(new Point(x, i));
             }
@@ -27,5 +38,11 @@ public class Bomb{
     }
     public int getY(){
         return y;
+    }
+    public void tick(){
+        time++;
+    }
+    public int getTime(){
+        return time;
     }
 }
